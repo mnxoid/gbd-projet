a8dfe53518e381cf5d6996cc74eb7c9ba1356f4c
Dudyak Michael <sergolwow@gmail.com>
2019-03-26 07:32:51 +0100

    Some imbrications #1

--- data_prep.js
+++ data_prep.js
@@ -5,23 +5,17 @@ function imbricate(parent, parent_field, child, child_field) {
 	if (fetchobj(db.getCollection(child).find({})).length > 0) {
 		print(`\n${child} et ${parent} fusion`)
 		fetchobj(db.getCollection(parent).find({}))
-			.map(x=>x._id)
+			.map(x => x._id)
 			.forEach(id => {
 				print("Processing id: " + id)
 				mats = fetchobj(db.getCollection(child).find(
 					{[parent_field]: id},
-					{ 
-						[parent_field]: 0
-					} 
+					{[parent_field]: 0}
 				))
 
 				db.getCollection(parent).update(
 					{"_id": id},
-					{
-						$set: {
-							[child_field]: mats
-						}
-					}
+					{$set: {[child_field]: mats}}
 				)
 			})
 
@@ -31,5 +25,6 @@ function imbricate(parent, parent_field, child, child_field) {
 	}
 }
 
-imbricate("Fournisseurs", "Fournisseur", "Materiaux", "Materiaux")
-imbricate("Entrepots", "Entrepot", "Stock", "Stock")
+imbricate("Fournisseurs", "Fournisseur", "Materiaux",   "Materiaux")
+imbricate("Entrepots",    "Entrepot",    "Stock",       "Stock")
+imbricate("Vetements",    "Vetements",   "Composition", "Composition")
